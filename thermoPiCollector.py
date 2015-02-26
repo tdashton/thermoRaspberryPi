3eed3fa3ea9bb186ec3ac064ffe2c95181199adf
Tyler <tdashton@gmail.com>
2015-02-26 23:10:28 +0100

    read protocol version 0 from threadedServer

--- thermoPiCollector.py
+++ thermoPiCollector.py
@@ -5,17 +5,23 @@ import logging
 import socket
 import string
 import threading
+import time
 import collectorMysql
 
 HOST = ''                 # Symbolic name meaning all available interfaces
-MAIN_PORT = 3030          # Arbitrary non-privileged port for connection negotiation
+MAIN_PORT = 2020          # Arbitrary non-privileged port for connection negotiation
 # (re)use following range of ports for establishing communication channels
 PORT_RANGE = [2021, 2022, 2023, 2024, 2025]
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind((HOST, MAIN_PORT))
+logging.basicConfig(filename='collector.log', level=logging.DEBUG)
 
+'''
+for the main server
+'''
 def init_server_socket():
+    logging.info("server listening on {0}".format(MAIN_PORT))
     serverSocket.listen(2)
     return serverSocket.accept()
 
@@ -36,45 +42,48 @@ class threadedServer (threading.Thread):
         self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.serverSocket.bind((HOST, self.listenPort))
         self.serverSocket.listen(1)
-        conn, addr = self.serverSocket.accept()
-        print 'worker listening on {0}'.format(self.listenPort)
+        logging.info('worker listening on {0}'.format(self.listenPort))
+        return self.serverSocket.accept()
 
     def run(self):
-        self.initial_setup()
+        conn, addr = self.initial_setup()
 
-        # data = conn.recv(1024)
-        # parsed = string.split(data, '|')
-        # parsed[-1] = parsed[-1].strip()
-        # if parsed[0] == '0':
-        #     # proto version 0
-        #     temp = parsed[3]
-        #     timestamp = parsed[1]
-        #     sensorName = parsed[2]
-        # else:
-        #     print "proto not recognized:"
-        #     print parsed
-        #     break
+        while 1:
+            data = conn.recv(1024)
+            logging.debug(data)
+            parsed = string.split(data, '|')
+            parsed[-1] = parsed[-1].strip()
+            if parsed[0] == '0':
+                # proto version 0
+                temp = parsed[3]
+                timestamp = parsed[1]
+                sensorName = parsed[2]
+            else:
+                logging.debug("proto not recognized:")
+                logging.debug(parsed)
+                break
 
-        # collectorMysql.connectToDatasource()
-        # collectorMysql.writeToDatasource(temp, timestamp, sensorName)
+            collectorMysql.connectToDatasource()
+            collectorMysql.writeToDatasource(temp, timestamp, sensorName)
+            if not data: break
+            #conn.sendall(data)
 
-        data = self.serverSocket.recv(128) # receive inital connect request
-        print data
-
-        print "closing {0}".format(self.listenPort)
+        logging.debug("Worker closing port {0}".format(self.listenPort))
         self.serverSocket.close()
-        # if not data: break
-        #conn.sendall(data)
+        PORT_RANGE.append(self.listenPort)
 
 while 1:
     conn, addr = init_server_socket()
-    logging.debug(addr)
-    logging.debug(conn)
+    logging.debug("processing request from {0} {1}.".format(addr[0], addr[1]))
     data = conn.recv(128) # receive inital connect request
-    print "data: " + data
-    if data.strip() == "CONNECT": # client wants to connect
+    logging.debug("data: " + data)
+    if data.strip() == "CONNECT CMD": # client wants to connect and perform a command
         port = PORT_RANGE.pop()
-        logging.debug(port)
+        conn.send("CONNECT ACK\nREADY\n\n")
+
+    if data.strip() == "CONNECT LOG": # client wants to connect and send logs
+        port = PORT_RANGE.pop()
+        logging.debug("port: ".format(port))
         conn.send("CONNECT ACK\nNEGOTIATE:{0}\n\n".format(port))
         server = threadedServer(port)
         server.start()
@@ -82,5 +91,6 @@ while 1:
     else:
         conn.send("WHA?")
 
+    time.sleep(1)
     conn.shutdown(socket.SHUT_RDWR)
     conn.close()
