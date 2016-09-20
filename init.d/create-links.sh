3be193adee9b84600ea550597ee8f7613fa55250
Tyler <tdashton@gmail.com>
2016-09-20 20:44:33 +0200

    update init scripts

--- init.d/create-links.sh
+++ init.d/create-links.sh
@@ -1,3 +1,7 @@
-sudo ln -s /home/pi/thermoRaspberryPi/init.d/thermopi-client.sh  /etc/init.d/thermopi-client.sh
-sudo ln -s /home/pi/thermoRaspberryPi/init.d/thermopi-controller.sh  /etc/init.d/thermopi-controller.sh
-sudo ln -s /home/pi/thermoRaspberryPi/init.d/thermopi-collector.sh  /etc/init.d/thermopi-collector.sh
+sudo ln -s /home/pi/thermoRaspberryPi/init.d/thermopi-client.sh  /etc/init.d/thermopi-03-client.sh
+sudo ln -s /home/pi/thermoRaspberryPi/init.d/thermopi-controller.sh  /etc/init.d/thermopi-02-controller.sh
+sudo ln -s /home/pi/thermoRaspberryPi/init.d/thermopi-collector.sh  /etc/init.d/thermopi-01-collector.sh
+
+sudo update-rc.d thermopi-03-client.sh defaults
+sudo update-rc.d thermopi-02-controller.sh defaults
+sudo update-rc.d thermopi-01-collector.sh defaults
--- init.d/thermopi-collector.sh
+++ init.d/thermopi-collector.sh
@@ -5,7 +5,7 @@
 # Required-Start:  $network 
 # Required-Stop:   $network 
 # Default-Start:   2 3 4 5
-# Default-Stop:    0 1 6
+# Default-Stop:
 # Short-Description: Start ThermoPi Collector daemon
 ### END INIT INFO
 
