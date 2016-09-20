b0f3cc8da820e937356b77e6cd7cb4eb5196d64a
Tyler <tdashton@gmail.com>
2016-09-20 19:54:01 +0200

    links

---
+++ init.d/create-links.sh
@@ -0,0 +1,2 @@
+sudo ln -s /home/pi/thermoRaspberryPi/init.d/thermopi-client.sh  /etc/init.d/thermopi-client.sh
+sudo ln -s /home/pi/thermoRaspberryPi/init.d/thermopi-controller.sh  /etc/init.d/thermopi-controller.sh
